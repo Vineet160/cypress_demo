e0735e367c56f5879eccb65745bc4837fbb09bac
Vineet Revanker <vineet_revanker@persistent.co.in>
2020-08-17 21:18:50 +0530

    cloned again,"npm install", and added couple of assertions

--- cypress/integration/examples/first_test.js
+++ cypress/integration/examples/first_test.js
@@ -1,6 +1,8 @@
 describe("My First Test", function () {
   it("Launch Google page and verify title", function () {
     cy.visit("https://www.google.com/");
+    cy.url().should('include', 'google')
+    cy.get('.RNmpXc').contains('I\'m Feeling Lucky')
     cy.get('input[name="q"]').type("ronaldo");
     //   cy.get('input[class="gNO89b"]').click
     cy.contains("Google Search").click();
